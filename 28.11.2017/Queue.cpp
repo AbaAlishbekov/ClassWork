f91672369de7729b346085740318d0dc81dbc6d3
АлишбековА <@223_6>
2017-11-28 21:03:56 +0600

    asd1

--- 28.11.2017/Queue.cpp
+++ 28.11.2017/Queue.cpp
@@ -1,5 +1,9 @@
 #include "stdafx.h"
 
+using namespace std;
+
+
+
 template<typename T>
 class Queue
 {
@@ -28,35 +32,47 @@ Queue<T>::~Queue()
 	delete[] queue;
 }
 template<typename T>
-void Queue<T>::enqueue(const T&) {
+void Queue<T>::enqueue(const T&t) {
 	if (cnt < size)
 	{
-		queue[++last] = t;
+		queue[last++] = t;
 		++cnt;
 		if (last > size)
 		{
 			last -= size + 1;
 		}
 	}
-	
+
 }
 
 template<typename T>
 T Queue<T>::dequeue() {
-	if (cnt < 0)
+	if (cnt > 0)
 	{
 		T temp = queue[first++];
-		cnt--;
+		--cnt;
 		if (first > size)
 		{
 			first -= size + 1;
 		}
+		return temp;
 	}
+
 }
 int main() {
 	Queue<int> queue;
 
+	queue.enqueue(1);
+	queue.enqueue(2);
+	queue.enqueue(3);
+	queue.enqueue(4);
+	queue.enqueue(5);
+	cout << queue.dequeue() << endl;
+	cout << queue.dequeue() << endl;
+	cout << queue.dequeue() << endl;
+	cout << queue.dequeue() << endl;
+	cout << queue.dequeue() << endl;
 
-
+	system("pause");
 	return 0;
 }
